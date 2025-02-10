7bd7b221db03e37a83df6fb5abf3f07f22228a1a
William Joseph Armentrout <warmentr@hydra17.eecs.utk.edu>
2025-02-09 23:42:35 -0500

    Is done

--- src/famtree.c
+++ src/famtree.c
@@ -1,10 +1,11 @@
 // William Armentrout
 // warmentr
 // Description:
-//
-//
+// The program constructs and manages a family tree using a Red-Black tree.
+// It goes through input lines describing relationships amd prints the family tree
+// while ensuring no inconsistencies.
 
-// errors here are fixed when compiling
+// Required libraries
 #include <stdlib.h>
 #include <string.h>
 #include "fields.h"
@@ -28,9 +29,9 @@ typedef struct {
 
 } Person;
 
-// large number of healper functions to make main 
-// easier to read
+// large number of healper functions to make main easier to read
 
+// finds a person in the tree by their name
 Person* find_person(JRB tree, char* name) {
 	
 	if (name == NULL)
@@ -42,6 +43,7 @@ Person* find_person(JRB tree, char* name) {
 	return (Person *)node->val.v;
 };
 
+// inserts a new person into the tree
 Person* insert_person(JRB tree, char* name) {
 	
 	Person *p = malloc(sizeof(Person));
@@ -52,6 +54,7 @@ Person* insert_person(JRB tree, char* name) {
 	return p;
 };
 
+// frees all memory allocated by persons in the tree
 void free_all_persons(JRB tree) {
 	
 	JRB temp;
@@ -60,17 +63,15 @@ void free_all_persons(JRB tree) {
 	jrb_traverse(temp, tree) {
 		person = (Person *)temp->val.v;
 		free(person->name);
-
 		if(person->father) free(person->father);
 		if(person->mother) free(person->mother);
-
 		free(person);
 	}
-
 	jrb_free_tree(tree);
 };
 
-void person_print_info(Person* person) {
+// prints information about one person
+void print_persons_info(Person* person) {
 
 	printf("%s\n", person->name);
 
@@ -111,31 +112,34 @@ void person_print_info(Person* person) {
 		 
 };
 
-int person_is_own_descenant(Person * person){
+// checks if a person input_struct their own decendent (finds if there input_struct a cycle)
+int detect_cycle(Person * person){
 	
-	// If person is visited and no children contain 
+	// If person input_struct visited and no children contain 
 	// the person return 0
 	if (person->visited == 1) return 0;
 
-	// If person is visited while searching it's own
+	// If person input_struct visited while searching it's own
 	// children return 1
 	if (person->visited == 2) return 1;
 
+	// mark as visited
 	person->visited = 2;
 
 	// loop and check for cycled descendants
 	Dllist dtemp;
 	dll_traverse(dtemp, person->children) {
 		Person *child = (Person *)dtemp->val.v;
-		if (person_is_own_descenant(child)) return 1;
+		if (detect_cycle(child)) return 1;
 	}
 
+	// mark as checked
 	person->visited = 1;
-
 	return 0;
 };
 
-int person_has_child(Person* person, char *child_name) {
+// checks if a person already has a specific child
+int is_parent_of(Person* person, char *child_name) {
 	
 	Dllist dtemp;
 	dll_traverse(dtemp, person->children) {
@@ -146,8 +150,9 @@ int person_has_child(Person* person, char *child_name) {
 	return 0;
 }
 
-int person_set_sex(Person* person, char sex) {
-	
+// Sets the sex of a person if it has not been assigned yet
+int validate_and_set_sex(Person* person, char sex) {
+	// returns 1 if gender being set mismatches with one already set
 	if (person->sex && person->sex != sex)
 		return 1;
 
@@ -155,19 +160,22 @@ int person_set_sex(Person* person, char sex) {
 	return 0;
 }
 
-int person_set_parent(Person* child, Person* parent, int father) {
+// assigns a parent to a child
+int assign_parent(Person* child, Person* parent, int father) {
 	
+	// checks for a gender conflict
 	if (parent->sex && parent->sex != (father ? 'M' : 'F')) {
 		return 2;
 	}
 
+	// 
 	if (father) {
 		
 		if(child->father == NULL) {
 			child->father = strdup(parent->name);
 			return 0;
 		} else if(strcmp(child->father, parent->name) != 0) {
-			// return 1 if child has father w/ different name
+			// return 1 if child has father w/ different name / multiple fathers
 			return 1;
 		} else {
 			return 0;
@@ -178,6 +186,7 @@ int person_set_parent(Person* child, Person* parent, int father) {
 			child->mother = strdup(parent->name);
 			return 0;
 		} else if (strcmp(child->mother, parent->name) != 0) {
+			// conflict with multiple mothers
 			return 1;
 		} else {
 			return 0;
@@ -186,14 +195,16 @@ int person_set_parent(Person* child, Person* parent, int father) {
 
 }
 
+// adds a child to a person's list of children
 void person_add_child(Person* parant, Person* child) {
 	
-	if (!person_has_child(parant, child->name)) {
+	if (!is_parent_of(parant, child->name)) {
 		dll_append(parant->children, new_jval_v((void *)child));
 	}
 }
 
-int person_parents_printed(JRB tree, Person* person) {
+// checks if both parents of a person have been printed
+int are_parents_printed(JRB tree, Person* person) {
 	
 	if (person->mother == NULL && person->father == NULL)
 		return 1;
@@ -209,23 +220,24 @@ int person_parents_printed(JRB tree, Person* person) {
 	return 1;
 }
 
-char* get_line_value(IS is) {
+// reads multi-word values from an input line
+char* extract_value_from_line(IS input_struct) {
 	int nsize;
 	char *value;
 
 	// find size of string
-	nsize = strlen(is->fields[1]);
-	for (int i = 2; i < is->NF; i++) nsize += (strlen(is->fields[i]) + 1);
+	nsize = strlen(input_struct->fields[1]);
+	for (int i = 2; i < input_struct->NF; i++) nsize += (strlen(input_struct->fields[i]) + 1);
 
 	// allocate memory
 	value = (char *)malloc(sizeof(char) * (nsize + 1));
-	strcpy(value, is->fields[1]);
+	strcpy(value, input_struct->fields[1]);
 
 	// 
-	nsize = strlen(is->fields[1]);
-	for (int i = 2; i < is->NF; i++) {
+	nsize = strlen(input_struct->fields[1]);
+	for (int i = 2; i < input_struct->NF; i++) {
 		value[nsize] = ' ';
-		strcpy(value + nsize + 1, is->fields[i]);
+		strcpy(value + nsize + 1, input_struct->fields[i]);
 		nsize += strlen(value + nsize);
 	}
 
@@ -236,23 +248,23 @@ int main() {
 
 	JRB tree, temp;
 	Dllist dtemp;
-	IS is;
+	IS input_struct;
 	Person *person;
 
 	int nsize;
 
 	// read input
-	is = new_inputstruct(NULL);
+	input_struct = new_inputstruct(NULL);
 	tree = make_jrb();
 
-	while (get_line(is) >= 0) {
+	while (get_line(input_struct) >= 0) {
 		// Skip unneccesarry lines
-		if (is->NF <= 1) continue;
+		if (input_struct->NF <= 1) continue;
 
-		// Keyword is the first word in line
-		char *keyword = is->fields[0];
+		// Keyword input_struct the first word in line
+		char *keyword = input_struct->fields[0];
 
-		char *value = get_line_value(is);
+		char *value = extract_value_from_line(input_struct);
 
 		if (strcmp(keyword, "PERSON") == 0) {
 			person = find_person(tree, value);
@@ -265,13 +277,13 @@ int main() {
 			// 0 if father 1 if mother
 			int father = strcmp(keyword, "FATHER_OF") == 0 ? 1 : 0;
 
-			if (person_set_sex(person, father ? 'M' : 'F') == 1) {
-				fprintf(stderr, "Bad input - sex mismatch on line %d\n", is->line);
+			if (validate_and_set_sex(person, father ? 'M' : 'F') == 1) {
+				fprintf(stderr, "Bad input - sex mismatch on line %d\n", input_struct->line);
 
 				// Since we're exiting, free memory
 				free_all_persons(tree);
 				free(value);
-				jettison_inputstruct(is);
+				jettison_inputstruct(input_struct);
 				exit(1);
 			}
 
@@ -279,29 +291,29 @@ int main() {
 			if (child == NULL) 
 				child = insert_person(tree, value);
 
-			int set_parent = person_set_parent(child, person, father);
+			int set_parent = assign_parent(child, person, father);
 			if(set_parent == 1) {
-				fprintf(stderr, "Bad input -- child with two %s on line %d\n", father ? "fathers": "mothers", is->line);
+				fprintf(stderr, "Bad input -- child with two %s on line %d\n", father ? "fathers": "mothers", input_struct->line);
 
 				// free memory
 				free_all_persons(tree);
 				free(value);
-				jettison_inputstruct(is);
+				jettison_inputstruct(input_struct);
 				exit(1);
 			} else if (set_parent == 2) {
-				fprintf(stderr, "Bad input - sex mismatch on line %d\n", is->line);
+				fprintf(stderr, "Bad input - sex mismatch on line %d\n", input_struct->line);
 
 				// free memory
 				free_all_persons(tree);
 				free(value);
-				jettison_inputstruct(is);
+				jettison_inputstruct(input_struct);
 				exit(1);
 			}
 
 			person_add_child(person, child);
 
 		} else if (strcmp(keyword, "FATHER") == 0 || strcmp(keyword, "MOTHER") == 0) {
-			// 0 is a father, 1 is a mother
+			// 0 input_struct a father, 1 input_struct a mother
 			int father = strcmp(keyword, "FATHER") == 0 ? 1 : 0;
 
 			Person *parent = find_person(tree, value);
@@ -310,44 +322,44 @@ int main() {
 			if (parent == NULL) 
 				parent = insert_person(tree, value);
 
-			if (person_set_sex(parent, father ? 'M' : 'F') == 1) {
-				fprintf(stderr, "Bad input - sex mismatch on line %d\n", is->line);
+			if (validate_and_set_sex(parent, father ? 'M' : 'F') == 1) {
+				fprintf(stderr, "Bad input - sex mismatch on line %d\n", input_struct->line);
 
 				//free memory
 				free_all_persons(tree);
 				free(value);
-				jettison_inputstruct(is);
+				jettison_inputstruct(input_struct);
 				exit(1);
 			}
 
-			int set_parent = person_set_parent(person, parent, father);
+			int set_parent = assign_parent(person, parent, father);
 			if (set_parent == 1) {
-				fprintf(stderr, "Bad input -- child with two %s on line %d\n", father ? "fathers" : "mothers", is->line);
+				fprintf(stderr, "Bad input -- child with two %s on line %d\n", father ? "fathers" : "mothers", input_struct->line);
 
 				//free memory
 				free_all_persons(tree);
 				free(value);
-				jettison_inputstruct(is);
+				jettison_inputstruct(input_struct);
 				exit(1);
 			} else if (set_parent == 2) {
-				fprintf(stderr, "Bad input - sex mismatch on line %d\n", is->line);
+				fprintf(stderr, "Bad input - sex mismatch on line %d\n", input_struct->line);
 
 				//free memory
 				free_all_persons(tree);
 				free(value);
-				jettison_inputstruct(is);
+				jettison_inputstruct(input_struct);
 				exit(1);
 			}
 
 			person_add_child(parent, person);
 		} else if (strcmp(keyword, "SEX") == 0) {
-			if (person_set_sex(person, is->fields[1][0]) == 1) {
-				fprintf(stderr, "Bad input - sex mismatch on line %d\n", is->line);
+			if (validate_and_set_sex(person, input_struct->fields[1][0]) == 1) {
+				fprintf(stderr, "Bad input - sex mismatch on line %d\n", input_struct->line);
 
 				//free memory
 				free_all_persons(tree);
 				free(value);
-				jettison_inputstruct(is);
+				jettison_inputstruct(input_struct);
 				exit(1);
 			}
 
@@ -361,12 +373,12 @@ int main() {
 	jrb_traverse(temp, tree) {
 		
 		person = (Person *)temp->val.v;
-		if (person_is_own_descenant(person)) {
+		if (detect_cycle(person)) {
 			fprintf(stderr, "Bad input -- cycle in specification\n");
 
 			//free memory
 			free_all_persons(tree);
-			jettison_inputstruct(is);
+			jettison_inputstruct(input_struct);
 			exit(1);
 		}
 	}
@@ -389,8 +401,8 @@ int main() {
 
 		if(!person->printed) {
 			// only print if both parents are printed
-			if(person_parents_printed(tree, person)) {
-				person_print_info(person);
+			if(are_parents_printed(tree, person)) {
+				print_persons_info(person);
 				person->printed = 1;
 
 				// add dhildren to list
@@ -404,7 +416,7 @@ int main() {
 		
 	// free all memory
 	free_all_persons(tree);
-	jettison_inputstruct(is);
+	jettison_inputstruct(input_struct);
 	free_dllist(print_list);
 
 	exit(0);
